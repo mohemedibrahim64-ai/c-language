6c2946a312b6bdf60d016d0bc1918201f2e49a3f
mohemedibrahim64-ai <mohemedibrahim64@gmail.com>
2026-08-13 00:39:47 +0530

    created a c language

---
+++ cv.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    char c;
+    printf("enter your first number");
+    scanf("%d",&a);
+    printf("enter your opperater");
+    scanf(" %c",&c);
+    printf("enter your second number");
+    scanf("%d",&b);
+
+}
