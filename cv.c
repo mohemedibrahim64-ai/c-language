ddf58e08c43925f2277a6ea854def557bef9c42a
mohemedibrahim64-ai <mohemedibrahim64@gmail.com>
2026-08-15 19:30:50 +0530

    shoping

--- cv.c
+++ cv.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 int main(){
     int item;
-    int rate;
+    int ;
     int quantity;
     printf("--------Welcome to the ammerican super market-------\n");
     printf("--------our products---------\n");
     printf("1.Apple\n2.Orange\n3.Banana\nTo select a item press the number");
 scanf("%d",&item);
 if (item ==1){
-    printf("");
+    printf("you are selected apple for rupees 110");
+}if (item ==2){
+    printf("you are selected orange for rupees 90");
+}if (item ==3){
+    printf("you are selected banana for rupees 70");
 }
+printf("please select the quantity ");
+scanf("%d",quantity);
+printf("total =%d",);
+
 
 
 }
