40ad53159b2adef816ff4382e9149b55d6c2d438
mohemedibrahim64-ai <mohemedibrahim64@gmail.com>
2026-08-16 12:21:09 +0530

    grade

---
+++ new.c
@@ -0,0 +1,15 @@
+/*student grade calculater*/
+#include <stdio.h>
+int main(){
+    float mark;
+    printf ("This a student grade calculater\nEnter your mark:");
+    scanf("%f",&mark);
+    if (mark <=35){
+printf("your grade is C");
+    }
+    if(mark <=50 ){
+        printf("your grade is C-");
+
+    }
+
+}
