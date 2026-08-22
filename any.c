72ebf291204d241e3e07852be8ac2b3abde3b1df
A Mohamed brahim <mohemedibrahim64@gmail.com>
2026-08-23 01:35:59 +0530

    ass

---
+++ any.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    char stname[30];
+    int age;
+    int mark;
+    printf("type your name:");
+    scanf(" %s\n",&stname);
+    printf("enter your age:");
+    scanf("%d",&age);
+    printf("enter your mark:");
+    printf("--------MARK MEMO----------\n");
+    printf("%s\n",stname);
+    printf("%d\n",age);
+    printf("%d\n",mark);
+
+
+
+
+}
