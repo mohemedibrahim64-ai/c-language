50f2448e1d9f5273a6eb465a7dff74b67f85a510
A Mohamed brahim <mohemedibrahim64@gmail.com>
2026-09-05 21:13:46 +0530

    afsad

--- any.c
+++ any.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-int main()
-{
-   int j=3.3*3.3;
-    printf("age=%d",j);
+int main(){
+    char section;
+    int year;
+    int age;
+    printf ("enter your section\n");
+    scanf(" %c",&section);
+    printf("enter your year\n");
+    scanf("%d",&year);
+    printf("enter your age\n");
+    scanf("%d",&age);
+    printf("your section is:%C\n",section);
+    printf("your year is:%d\n",year);
+    printf("your age is:%d\n",age);
+return 0;
 }
