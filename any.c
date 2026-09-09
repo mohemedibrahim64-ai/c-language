793b85d40795421b4110897fff033fc7ed9cbe9b
A Mohamed brahim <mohemedibrahim64@gmail.com>
2026-09-09 21:47:24 +0530

    fef

--- any.c
+++ any.c
@@ -1,20 +1,25 @@
+//calculater using two digits  
 #include <stdio.h>
-int main(){
-    int time;
-    printf("welcome to the day\n");
-    scanf("%d",&time);
-    if (time<=11){
-        printf("very good morning");
+int main (){
+int num1;
+int num2;
+char v;
+printf("enter your first num\t");
+scanf("%d",&num1);
+printf("enter yor second num\t");
+scanf("%d",&num2);
+printf("select your character(+,-,*,/)\t");
+scanf(" %c",&v);
+if (v=='+'){
+    printf("result=%d",num1+num2);
+}if (v=='-'){
+    printf("result=%d",num1-num2);
+}if (v=='*'){
+    printf("result=%d",num1*num2);
+}if (v=='/'){
+    printf("result=d",num1/num2);
+}
+return 0;
 
-    }
-    if(time >=12){
-        printf("good after noon");
-    }
-    if (time =6){
-        printf("good evening");
 
-    }
-    else {
-        printf("good night");
-    }
 }
--- billing software.c
+++ billing software.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-int main(){
-    char section;
-    int year;
-    int age;
-    printf ("enter your section\n");
-    scanf(" %c",&section);
-    printf("enter your year\n");
-    scanf("%d",&year);
-    printf("enter your age\n");
-    scanf("%d",&age);
-    printf("your section is:%C\n",section);
-    printf("your year is:%d\n",year);
-    printf("your age is:%d\n",age);
-return 0;
-}
+#include <
